14418684b49a29b357bfe74fc5dc405eb872c0c7
Princeton Gomes <princeton4d@gmail.com>
2022-06-11 17:18:14 +0530

    modifications

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 require("dotenv").config()
-const { Pool } = require('pg');
 global.env = process.env
-const connection_string = env["CONNECTION_STRING"]
+const connection_string = "postgres://vyunohcqovugvm:a47bad4da5e8e4005323ecfe0d8bab087d7d0785af12eee53695f0611e7588fb@ec2-54-228-32-29.eu-west-1.compute.amazonaws.com:5432/dbd6nsjqeb6qfs"
+const { Pool } = require('pg');
 
 const pool = new Pool({
     connection_string
