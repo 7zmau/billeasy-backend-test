cd748a638acc1814ca0c99734f745cc22da56320
Princeton Gomes <princeton4d@gmail.com>
2022-06-11 17:34:43 +0530

    fix db connection

--- db.js
+++ db.js
@@ -1,10 +1,13 @@
 require("dotenv").config()
 global.env = process.env
-const connection_string = "postgres://vyunohcqovugvm:a47bad4da5e8e4005323ecfe0d8bab087d7d0785af12eee53695f0611e7588fb@ec2-54-228-32-29.eu-west-1.compute.amazonaws.com:5432/dbd6nsjqeb6qfs"
+const connection_string = env["CONNECTION_STRING"]
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    connection_string
+    connectionString: connection_string,
+    ssl: {
+        rejectUnauthorized: false
+    }
 })
 
 module.exports = { pool };
