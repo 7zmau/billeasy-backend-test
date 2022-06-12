abaaa1f4c7153795ac1e66e0f9d7547644501e87
Princeton Gomes <princeton4d@gmail.com>
2022-06-12 23:49:50 +0530

    fix env variable name

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 require("dotenv").config()
 global.env = process.env
-const connection_string = env["CONNECTION_STRING"]
+const connection_string = env["DATABASE_URL"]
 const { Pool } = require('pg');
 
 const pool = new Pool({
